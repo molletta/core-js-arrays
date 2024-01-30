21b3851953e596e4584e16dc2f88d28160bec8be
molletta <yasyule4ka@mail.ru>
2024-01-30 17:04:58 +0300

    feat: add solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,13 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  const step = 1;
+  const newArr = Array.from(
+    { length: (end - start) / step + 1 },
+    (value, index) => start + index * step
+  );
+  return newArr;
 }
 
 /**
